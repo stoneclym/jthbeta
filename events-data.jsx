72b69a3e98e2f7f7b192f5e731044938f67bcb52
stoneclym <133927098+stoneclym@users.noreply.github.com>
2026-05-29 00:00:30 -0400

    Update events from Google Sheets (2 events)

--- events-data.jsx
+++ events-data.jsx
@@ -1,5 +1,5 @@
 // events-data.jsx ? Auto-generated. Do not edit manually.
-// Last updated: 5/28/2026, 11:54:12 PM ET
+// Last updated: 5/29/2026, 12:00:30 AM ET
 
 const UPCOMING_EVENTS = [
   {
@@ -9,6 +9,14 @@ const UPCOMING_EVENTS = [
     "detail": "Testing to see if Claude knows what it's doing",
     "spots": "2",
     "url": "https://www.apple.com/"
+  },
+  {
+    "title": "Donation Drive",
+    "day": "21",
+    "mo": "JUNE",
+    "detail": "Donation for chocolate chip cookies.",
+    "spots": "100",
+    "url": "https://www.target.com/"
   }
 ];
 
